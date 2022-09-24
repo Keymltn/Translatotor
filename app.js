ca628632d64fdb2bc6ec885fa228eba25acb3bd7
Keymltn <gulnozarahmatova21@gmail.com>
2022-09-24 23:58:52 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,69 @@
+let inputEl = document.getElementById("input");
+let dictionerEl = document.getElementById("dictioner");
+let btnEl = document.getElementById("btn");
+let carEl = document.getElementById("car");
+let personEl = document.getElementById("person");
+let busEl = document.getElementById("bus");
+let laptopEl = document.getElementById("laptop");
+let bookEl = document.getElementById("book");
+
+btnEl.addEventListener("click" , (e) => {
+    e.preventDefault();
+    switch(inputEl.value.toLowerCase()){
+        case "car" && "mashina":
+            carEl.classList.toggle("block_el");
+        break;
+        case "person" && "inson":
+            personEl.classList.toggle("block_el");
+        break;
+        case "bus" && "avtobus":
+            busEl.classList.toggle("block_el");
+        break;
+        case "laptop" && "noutbuk":
+            laptopEl.classList.toggle("block_el");
+        break;
+        case "book" && "kitob":
+            bookEl.classList.toggle("block_el");
+        break;
+    }
+})
+
+
+
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
+// ===========FUNCTIONS===============
